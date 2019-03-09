545f05e3ef104c6a5a96cbe96a113fa16b575a74
Tyler Murphy <tjames7000@gmail.com>
2019-03-09 10:56:08 -0500

    Subscribes to all motion sensors on updates. Previously, the subscription was only set up in response to install events.

--- lifecycleHandlers/update.ts
+++ lifecycleHandlers/update.ts
@@ -3,8 +3,15 @@ import {
     UpdateRequest,
     UpdateResponse
 } from '../types'
+import { subscribeToAllMotionSensors } from '../smartThingsApi';
 
 const updateHandler: Handler<UpdateRequest, UpdateResponse> = async function(request) {
+    await subscribeToAllMotionSensors(
+        request.updateData.authToken,
+        request.updateData.installedApp.installedAppId,
+        request.updateData.installedApp.locationId
+    )
+
     return {
         updateData: {}
     }
